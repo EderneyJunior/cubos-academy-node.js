0e86e4c62e0ef85240cf51aa01e06467a37f274e
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-24 18:39:59 -0300

    Exercicio 10 resolvido

---
+++ exercicios-backend-variaveis/10/index.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 5
+let c = 1
+let delta = 0
+
+console.log(`Formula: ${a}x + ${b}y + ${c} = 0`)
+console.log('RESULTADO:')
+
+delta = b**2 - (4*a*c)
+
+console.log(`O valor de delta é: ${delta}`)
