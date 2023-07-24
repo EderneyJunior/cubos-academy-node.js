21af26b22518479189ec4d5269a3e6f0492fee6e
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-24 18:30:14 -0300

    Exercicio 09 resolvido

---
+++ exercicios-backend-variaveis/09/index.js
@@ -0,0 +1,5 @@
+let numLados = 6
+let soma = (numLados - 2) * 180
+let valorAngulo = soma / numLados
+
+console.log(`O valor da soma dos ângulos internos foi ${soma} e o valor de cada ângulo do polígono foi ${valorAngulo}`)
