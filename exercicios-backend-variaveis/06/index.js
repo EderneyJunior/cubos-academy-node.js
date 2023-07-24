c43e63a53e2db68f8e9aa5d173ba713acc5476b8
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-24 18:00:20 -0300

    Exercicio 06 resolvido

---
+++ exercicios-backend-variaveis/06/index.js
@@ -0,0 +1,7 @@
+let disPercorrida = 500
+let tempo = 10
+
+let velocidade = disPercorrida / tempo
+velocidade *= 3.6
+
+console.log(`Ao percorrer ${disPercorrida}m em ${tempo}s sua velocidade média foi ${velocidade}km/h`)
