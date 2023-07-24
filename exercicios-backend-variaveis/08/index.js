eb94846f2d58c04e7d8b8a1d288ad346d03c66d8
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-24 18:21:04 -0300

    Exercicio 08 resolvido

---
+++ exercicios-backend-variaveis/08/index.js
@@ -0,0 +1,5 @@
+let raio = 3
+let comprimento = 2 * 3.14 * raio
+let area = 3.14 * raio**2
+
+console.log(`Com o raio sendo ${raio} temos um comprimento ${comprimento.toFixed(2)} e uma área de ${area.toFixed(2)}`)
