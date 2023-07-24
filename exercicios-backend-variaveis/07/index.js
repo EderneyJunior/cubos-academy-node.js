113274e9d8aaebf1aec6f049fc649e9b2185f8de
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-24 18:10:18 -0300

    Exercicio 07 resolvido

---
+++ exercicios-backend-variaveis/07/index.js
@@ -0,0 +1,5 @@
+let po = 1000, x = 4, t = 7
+
+let p = po * x**(t/7)
+
+console.log(`Após ${t} dias, o total de pessoas infectadas será de ${p.toFixed(1)}, uma vez que inicialmente existiam ${po} pessoas infectadas.`)
