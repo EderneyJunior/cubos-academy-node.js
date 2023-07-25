1bc4ba8004e3956437ae2b7653968dbe1c093b90
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 17:40:20 -0300

    Exercicio 07 feito

--- exercicios-backend-condicionais/07/index.js
+++ exercicios-backend-condicionais/07/index.js
@@ -1,5 +1,15 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 2700000; //emCentavos
 
 //seu código aqui
+
+if (totalDeRendimentos > 2855970 && !portadoraDeDoenca && !aposentada) {
+    console.log('PEGA LEÃO')
+} else {
+    if (aposentada || portadoraDeDoenca) {
+        console.log('ISENTA')
+    } else if (totalDeRendimentos < 2855970) {
+        console.log('VAZA LEAO! JA TA DIFICIL SEM VOCE')
+    }
+}
