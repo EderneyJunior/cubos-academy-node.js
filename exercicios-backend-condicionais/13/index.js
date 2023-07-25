4033322b57e6d96bdf16bc150327959f45682f8c
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 19:30:43 -0300

    Exercicio 13 feito

--- exercicios-backend-condicionais/13/index.js
+++ exercicios-backend-condicionais/13/index.js
@@ -4,3 +4,17 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+/*
+| Forma de Pagamento | Desconto |
+| ------------------ | -------- |
+| credito            | 5%       |
+| cheque             | 3%       |
+| debito ou dinheiro | 10%      |
+*/
+
+let valorFinal = 0
+
+if (tipoDePagamento == 'credito') {
+   valorFinal = (valorDoProduto - valorDoProduto * 0.05) * 0.01
+console.log(`Valor a ser pago: ${valorFinal.toFixed(2)}`)
+}
