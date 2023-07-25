043aeb8f4275ca106eeeea6f493ed1f245f21719
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 17:26:52 -0300

    Exercicio 06 feito

--- exercicios-backend-condicionais/06/index.js
+++ exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,23 @@
-const alturaEmCm = 185;
+const alturaEmCm = 206;
 
 //seu código aqui
+
+/*
+- Caso o jogador tenha menos de 180cm, o programa deve imprimir **REPROVADO**
+- Caso tenha entre 180cm e 185cm, o programa deve imprimir **LÍBERO**
+- Caso tenha entre 186cm e 195cm, o programa deve imprimir **PONTEIRO**
+- Caso tenha entre 196cm e 205cm, o programa deve imprimir **OPOSTO**
+- Caso tenha mais de 205cm, o programa deve imprimir **CENTRAL**
+*/
+
+if (alturaEmCm < 180) {
+    console.log('REPROVADO')
+} else if (alturaEmCm < 186) {
+    console.log('LÉBERO')
+} else if (alturaEmCm < 196) {
+    console.log('PONTEIRO')
+} else if (alturaEmCm < 206) {
+    console.log('OPOSTO')
+} else {
+    console.log('CENTRAL')
+}
