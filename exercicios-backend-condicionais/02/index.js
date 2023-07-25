56c5ffe2ec49fa7f2c1854a3eb613f7be8daa5de
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 16:59:54 -0300

    Exercicio 02 feito

--- exercicios-backend-condicionais/02/index.js
+++ exercicios-backend-condicionais/02/index.js
@@ -1,4 +1,9 @@
-const jogada1 = 5;
-const jogada2 = 3;
+const jogada1 = 4;
+const jogada2 = 4;
 
 //seu código aqui
+if ((jogada1 + jogada2) % 2 == 0){
+    console.log('par')
+} else {
+    console.log('impar')
+}
