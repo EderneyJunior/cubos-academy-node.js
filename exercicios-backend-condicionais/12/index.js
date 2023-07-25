18844e819d4e58a90efe58587f33931e6b44dba7
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 19:20:38 -0300

    Exercicio 12 letra A

--- exercicios-backend-condicionais/12/index.js
+++ exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,17 @@
-const diaDaSemana = 3;
+const diaDaSemana = 5;
+
+if (diaDaSemana == 1) {
+    console.log('Segunda-Feira')
+} else if (diaDaSemana == 2) {
+    console.log('Terça-Feira')
+} else if (diaDaSemana == 3) {
+    console.log('Quarta-Feira')
+} else if (diaDaSemana == 4) {
+    console.log('Quinta-Feira')
+} else if (diaDaSemana == 5) {
+    console.log('Sexta-Feira')
+} else if (diaDaSemana == 6) {
+    console.log('Sábado')
+} else {
+    console.log('Domingo')
+}
