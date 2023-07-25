a66a2ad281c3a4884b0fb2324dd5fe683f1fb466
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 18:18:28 -0300

    Exercicio 10 feito

--- exercicios-backend-condicionais/10/index.js
+++ exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,19 @@
-const caractere = "E";
+const caractere = "U";
+const num = Number(caractere)
+/*
+| Categoria | Caractere                             |
+| --------- | ------------------------------------- |
+| Vogal     | A E I O U a e i o u                   |
+| Consoante | Qualquer letra que não seja uma vogal |
+| Número    | 1 2 3 4 5 6 7 9 0                     |
+*/
+
+if (caractere == 'z' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u') {
+    console.log('Vogal minúscula')
+} else if (caractere == 'A' || caractere == 'E' || caractere == 'I' || caractere == 'O' || caractere == 'U') {
+    console.log('Vogal maiúscula')
+} else if (Number(caractere) === num) {
+    console.log('Número')
+} else {
+    console.log('Consoante')
+}
