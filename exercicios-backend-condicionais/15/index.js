94b9e6200b4c560948959cd4f43cecc433fcd378
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 19:55:47 -0300

    Exercicio 15 feito

--- exercicios-backend-condicionais/15/index.js
+++ exercicios-backend-condicionais/15/index.js
@@ -1,4 +1,11 @@
 const primeiroNome = "Mario";
-const sobrenome = "";
-const apelido = "";
+const sobrenome = "Bross";
+const apelido = "Silva";
 
+if (apelido) {
+    console.log(apelido)
+} else if (sobrenome) {
+    console.log(primeiroNome + ' ' + sobrenome)
+} else {
+    console.log(primeiroNome)
+}
