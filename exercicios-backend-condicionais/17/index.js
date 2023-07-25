11d26ad12f5571c5755c2175a0bea7708ab8e23b
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 20:47:21 -0300

    Exercicio 17 feito

--- exercicios-backend-condicionais/17/index.js
+++ exercicios-backend-condicionais/17/index.js
@@ -6,3 +6,14 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+let valorParcela = (valorDoProduto * 0.01) / quantidadeDoParcelamento
+let c = valorParcela
+let parcelas = 0
+
+while(c <= valorPago) {
+    c += valorParcela
+    parcelas += 1
+}
+
+console.log(`Restam ${quantidadeDoParcelamento - parcelas} parcelas de R$${valorParcela}`)
