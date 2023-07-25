2c9cae09713218f84d3ece068b94ab31b9fad74b
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 19:59:21 -0300

    Exercicio 16 feito

--- exercicios-backend-condicionais/16/index.js
+++ exercicios-backend-condicionais/16/index.js
@@ -1,5 +1,11 @@
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+const idadeDoAluno = 18;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+
+if (idadeDoAluno >= 18 || possuiResponsavel) {
+    console.log('Rematrícula realizada com sucesso.')
+} else {
+    console.log('Não é possivel fazer a rematrícula.')
+}
