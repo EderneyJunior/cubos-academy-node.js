384d6b0d36fc1b1874ccd88647f8c0eb1cc23e7f
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-25 16:55:45 -0300

    Exercicio 01 feito

--- exercicios-backend-condicionais/01/index.js
+++ exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,20 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "tesoura"
+const jogada2 = "papel"
 
 //seu código aqui
+
+if (jogada1 == 'pedra' && jogada2 == 'tesoura'){
+    console.log('pedra')
+} else if (jogada1 == 'pedra' && jogada2 == 'papel'){
+    console.log('papel')
+} else if (jogada1 == 'tesoura' && jogada2 == 'papel'){
+    console.log('tesoura')
+} else if (jogada1 == 'papel' && jogada2 == 'tesoura'){
+    console.log('tesoura')
+} else if (jogada1 == 'papel' && jogada2 == 'pedra'){
+    console.log('papel')
+} else if (jogada1 == 'tesoura' && jogada2 == 'pedra'){
+        console.log('pedra')
+} else if (jogada1 == jogada2) {
+    console.log('empate')
+}
