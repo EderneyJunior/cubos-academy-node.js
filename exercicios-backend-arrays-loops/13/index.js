eaa67cc20c59afd2d07c3c8d03078b4ddfc790e6
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 18:15:08 -0300

    Exercicio 13

--- exercicios-backend-arrays-loops/13/index.js
+++ exercicios-backend-arrays-loops/13/index.js
@@ -1,2 +1,7 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+const disjuntores = [true, false, true, false, false, true, false, false];
 
+for (let i = 0; i < disjuntores.length; i++) {
+    if (disjuntores[i]) {
+        console.log(i)
+    }
+}
