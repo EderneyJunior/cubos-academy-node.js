3908d56cdcae6dd0f86effaf64ab67aa9b507ec2
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 17:35:09 -0300

    Exercicio 11

--- exercicios-backend-arrays-loops/11/index.js
+++ exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,10 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 13, 17, 26, 343, 118, 245];
+const entre = []
 
+for (let i of original) {
+    if (i >= 10 && i <= 20 || i > 100) {
+        entre.push(i)
+    }
+}
+
+console.log(entre)
