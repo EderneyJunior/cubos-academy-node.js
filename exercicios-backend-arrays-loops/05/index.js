0d9ef68846f52eb39e1f6bc012188f824b9a23d5
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 17:13:10 -0300

    Exercicio 05

--- exercicios-backend-arrays-loops/05/index.js
+++ exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,10 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 3, 12, 21, 54, 88, 112];
+const pares = []
+
+for (let i of original) {
+    if (i % 2 == 0) {
+        pares.push(i)
+    }
+}
+
+console.log(pares)
