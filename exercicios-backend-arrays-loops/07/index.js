8ffab09de0b38381fe9d063f9050acb228b26324
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 17:21:18 -0300

    Exercicio 07

--- exercicios-backend-arrays-loops/07/index.js
+++ exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,10 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const letraA = []
+
+for (let i of nomes) {
+    if (i[0] == 'A' || i[0] == 'a') {
+        letraA.push(i)
+    }
+}
+
+console.log(letraA)
