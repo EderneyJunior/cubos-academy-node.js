120bd8b213331eb5d57301479b62070ffff95cbb
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 18:04:43 -0300

    Exercicio 12

--- exercicios-backend-arrays-loops/12/index.js
+++ exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,17 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+//const filaDeDentro = ["Jose", "Maria", "Joao"];
+//const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
+
+while (filaDeDentro.length < 5) {
+    if (filaDeFora.length == 0) {
+        break
+    } else {
+    filaDeDentro.push(filaDeFora[0])
+    filaDeFora.shift()
+    }
+}
+
+    console.log(filaDeDentro)
+    console.log(filaDeFora)
