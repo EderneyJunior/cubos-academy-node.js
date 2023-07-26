93d464c87c0158c55421f9e5331b7f355e33e2e4
Ederney Pedroso Junior <135053371+EderneyJunior@users.noreply.github.com>
2023-07-26 17:16:39 -0300

    Exercicio 06

--- exercicios-backend-arrays-loops/06/index.js
+++ exercicios-backend-arrays-loops/06/index.js
@@ -1,2 +1,10 @@
-const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const numeros = [2, 4, 7, 8, 1, 6, 5, 10];
+let soma = 0
 
+for (let i of numeros) {
+    if (i % 2 == 0) {
+        soma += i
+    }
+}
+
+console.log(soma)
