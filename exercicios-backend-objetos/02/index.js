4175ef1f706a76b4faa19fabdf842ec2d9aea28d
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-07-31 18:51:01 -0300

    Exercicio 2 arrumado

--- exercicios-backend-objetos/02/index.js
+++ exercicios-backend-objetos/02/index.js
@@ -1,4 +1,4 @@
-const carro = {
+const carro1 = {
     marca: 'Toyota',
     modelo: 'Corolla',
     ano: 2014,
@@ -7,4 +7,28 @@ const carro = {
     automatico: true 
 }
 
-console.log(carro)
+const carro2 = {
+    marca: "Volkswagen",
+    modelo: "Gol",
+    ano: 2022,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false
+}
+
+const carro3 = {
+    marca: "Volkswagen",
+    modelo: "Saveiro",
+    ano: 2014,
+    cor: "preto",
+    quantidade_portas: 2,
+    automatico: false
+}
+
+const carros = {
+    carro1,
+    carro2,
+    carro3
+}
+
+console.log(carros)
