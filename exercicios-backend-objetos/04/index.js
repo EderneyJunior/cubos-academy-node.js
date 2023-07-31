fd5e870241eaedd0cfe9f15656e36e7e1ee780f7
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-07-31 15:47:00 -0300

    Exercicio 04

---
+++ exercicios-backend-objetos/04/index.js
@@ -0,0 +1,27 @@
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+]
+
+for (let item of usuarios) 
+    item['maior_idade'] = item.idade >= 18 ? item['maior_idade'] = true : item['maior_idade'] = false
+
+console.log(usuarios)
