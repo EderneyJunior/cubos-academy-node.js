1ea743a7f83bd6ed1fbe4dc03567b4a67fe0de43
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-07-31 15:36:46 -0300

    Exercicio 03

---
+++ exercicios-backend-objetos/03/index.js
@@ -0,0 +1,10 @@
+//`Olá... Eu sou João e tenho 25 anos de idade. Sou programador e minha cor preferida é verde!`
+
+const usuario = {
+    nome: "Ederney",
+    idade: 20,
+    profissao: "programador",
+    cor_preferida: "preto",
+}
+
+console.log(`Olá... Eu sou ${usuario.nome} e tenho ${usuario.idade} anos de idade. Sou ${usuario.profissao} e minha cor preferida é ${usuario.cor_preferida}`)
