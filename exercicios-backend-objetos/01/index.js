e411ea155ae3d8b7204c39a7b20f89dbe96e07cd
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-07-31 15:28:42 -0300

    Exercicio 01

---
+++ exercicios-backend-objetos/01/index.js
@@ -0,0 +1,24 @@
+const endereco = {
+    rua: 'Amadeu Gobato',
+    n: 35,
+    complemento: 'casa',
+    bairro: 'centro',
+    pais: 'brasil',
+    estado: 'PR',
+    cep: 86375000,
+    cidade: 'itambaracá'
+}
+/*   - nome
+    - email
+    - telefone
+    - data_nascimento
+    - endereco
+*/
+const usuario = {
+    nome: 'Ederney',
+    email: 'pedrosoederney@gmail.com',
+    tel: 43991115092,
+    nasc: '12/05/2003',
+    endereco
+}
+console.log(usuario)
