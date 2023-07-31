f9670f034f2843fdc96169380e33e0b79b2ab92a
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-07-31 16:02:38 -0300

    Exercicio 05

---
+++ exercicios-backend-objetos/05/index.js
@@ -0,0 +1,27 @@
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [],
+}
+
+curso.aulas.push({
+    Identificador: 1,
+    nomeDaAula: 'Introdução a programação'
+})
+
+curso.aulas.push({
+    Identificador: 2,
+    nomeDaAula: 'Variáveis'
+})
+
+curso.aulas.push({
+    Identificador: 3,
+    nomeDaAula: 'Condicionais'
+})
+
+curso.aulas.push({
+    Identificador: 4,
+    nomeDaAula: 'Arrays'
+})
+
+console.log(curso)
