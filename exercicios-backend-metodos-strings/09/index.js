6dc18af7aba6ad7a8d972bfe9cd9235f9bb48541
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 16:46:21 -0300

    Exercicio 09 resolvido

--- exercicios-backend-metodos-strings/09/index.js
+++ exercicios-backend-metodos-strings/09/index.js
@@ -1,1 +1,19 @@
-const nome = 'Guido Cerqueira';
+const nome = 'Ederney Junior';
+
+function nickName(nome) {
+    let array = nome.trim().split(' ')
+    let nick = ''
+    let nickCorreto = '@'
+    for (let item of array) {
+        nick += item
+    }
+    nick = nick.toLowerCase()
+
+    for (let letra of nick) {
+        nickCorreto += nickCorreto.length < 13 ? letra : ''
+    }
+
+    return nickCorreto
+}
+
+console.log(nickName(nome))
