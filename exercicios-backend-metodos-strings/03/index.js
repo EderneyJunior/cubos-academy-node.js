af709706ed5e04fcee6129a04f8bad65b89e68a0
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 15:01:29 -0300

    Exercicio 03 resolvido

--- exercicios-backend-metodos-strings/03/index.js
+++ exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,10 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+const array = texto.split(' ')
+let url = ''
+
+for (let i = 0; i < array.length; i++) {
+    url += i == 0 ? `${array[i]}` : `${array[i].padStart(array[i].length + 1, '-')}`
+}
+
+url = url.toLowerCase()
+console.log(url)
