c9e3a39ecee75ba5047e2ee3ed487da6aec13d79
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 16:17:17 -0300

    Exercicio 07 resolvido

--- exercicios-backend-metodos-strings/07/index.js
+++ exercicios-backend-metodos-strings/07/index.js
@@ -1,1 +1,11 @@
 const email = "aluno@cubos.academy";
+
+if (email[0] == '.' || email[email.length - 1] == '.') {
+    console.log('E-mail inválido')
+} else if (email.indexOf('@') == -1) {
+    console.log('E-mail inválido')
+} else if (email.lastIndexOf('.') < email.indexOf('@')) {
+    console.log('E-mail inválido')
+} else {
+    console.log('E-mail válido')
+}
