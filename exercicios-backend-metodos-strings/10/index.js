5727d5c264171c502690631a751e883901ac6b59
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 16:53:30 -0300

    Exercicio 10 resolvido

--- exercicios-backend-metodos-strings/10/index.js
+++ exercicios-backend-metodos-strings/10/index.js
@@ -1,1 +1,11 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+function arquivo(nomeArquivo) {
+    if (nomeArquivo.includes('jpg') || nomeArquivo.includes('jpeg') ||  nomeArquivo.includes('gif') || nomeArquivo.includes('png')) {
+        return 'Arquivo válido'
+    } else {
+        return 'Arquivo inválido'
+    }
+}
+
+console.log(arquivo(nomeArquivo))
