b1af4c2813dd6cda1138be4ee880d2f8629f9679
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 15:33:21 -0300

    Exercicio 05 resolvido

--- exercicios-backend-metodos-strings/05/index.js
+++ exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,6 @@
 const numeroCartao = '1111222233334444';
+const primeiros = numeroCartao.slice(0, 4)
+const ultimos = numeroCartao.slice(-4)
+const cartaoFormatado = `${primeiros}${ultimos.padStart(12, '*')}`
+
+console.log(cartaoFormatado)
