18cb54031a7d520c8eaf27deece8178541f74aa1
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 16:34:27 -0300

    Exercicio 08 resolvido

--- exercicios-backend-metodos-strings/08/index.js
+++ exercicios-backend-metodos-strings/08/index.js
@@ -1,1 +1,12 @@
 const cpf = "011.022.033-44";
+
+function tirarPontuação(cpf) {
+    let cpfSemPontuacao = ''
+    for (i = 0; i < 3; i++) {
+        cpfSemPontuacao = i == 0 ? cpf.replace('.', '') : cpfSemPontuacao.replace('.', '')
+        cpfSemPontuacao = i == 2 ? cpfSemPontuacao.replace('-', '') : cpfSemPontuacao
+    }
+    return cpfSemPontuacao
+}
+
+console.log(tirarPontuação(cpf))
