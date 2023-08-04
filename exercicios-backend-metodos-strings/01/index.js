2b6db6783ce55220981d0e3cf8d1e69f57407a6f
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-04 13:57:03 -0300

    Exercicio 01 resolvido

--- exercicios-backend-metodos-strings/01/index.js
+++ exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,7 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+if ((comentario.toUpperCase()).includes('COVID') || (comentario.toUpperCase()).includes('PANDEMIA')) {
+    console.log(`Comentário bloqueado por conter palavras proibidas`)
+} else {
+    console.log(`Comentário autorizado`)
+}
