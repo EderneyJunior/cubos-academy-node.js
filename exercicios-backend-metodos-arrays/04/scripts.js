d5d1e006ec4e4f9f30b9eed4e7b1974cd13c239b
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-07 13:49:31 -0300

    Exercicio 04 resolvido

--- exercicios-backend-metodos-arrays/04/scripts.js
+++ exercicios-backend-metodos-arrays/04/scripts.js
@@ -1,1 +1,15 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function clinica(array, operacao, nome = '') {
+    if (operacao == 'agendar') {
+        array.push(nome)
+    } else if (operacao == 'atender') {
+        array.shift()
+    }
+    const novaLista = array.join()
+    console.log(novaLista)
+    console.log('-=-=-=-=-=-=-=-=--=--=-=-=-=-=-===--=-=')
+}
+
+clinica(pacientes, 'agendar', 'Ederney')
+clinica(pacientes, 'atender')
