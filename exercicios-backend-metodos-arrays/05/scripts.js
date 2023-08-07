f9c8b32c258a59ddfcb1fd17df95b6d54ae2c321
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-08-07 14:14:17 -0300

    Exercicio 05 resolvido

--- exercicios-backend-metodos-arrays/05/scripts.js
+++ exercicios-backend-metodos-arrays/05/scripts.js
@@ -1,1 +1,33 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function agendarPaciente(lista, nome = '') {
+    if(nome == '') {
+        console.log('Digite o nome do paciente!')
+    } else {
+        lista.push(nome)
+        console.log(lista.join())
+    }
+}
+
+function atenderPaciente(lista) {
+    if (lista.length == 0) {
+        console.log('Não à ninguem para ser atendido!')
+    } else {
+        lista.shift()
+        console.log(lista.join())
+    }
+}
+
+function cancelarAtendimento(lista, nome = '') {
+    if (nome == '') {
+        console.log('Digite o nome do paciente que quer cancelar o atendimento!')
+    } else {
+        const posicao = lista.indexOf(nome)
+        lista.splice(posicao, 1)
+        console.log(lista.join())
+    }
+}
+
+agendarPaciente(pacientes, 'ederney')
+atenderPaciente(pacientes)
+cancelarAtendimento(pacientes, 'ederney')
