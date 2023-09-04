ea7e029e1f4da106b1fd239efe1973607a03dd8f
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-09-04 19:07:35 -0300

    Exercicio 04 resolvido e 03 corrigido

--- modulo-2/back-integral-datas-em-js/classe-04/index.js
+++ modulo-2/back-integral-datas-em-js/classe-04/index.js
@@ -1,18 +1,21 @@
 const taAberto = (entrada) => {
     const hora = entrada.getHours()
     const min = entrada.getMinutes()
+    const diaSemana = entrada.getDay()
 
     if (hora >= 8 && hora <= 18) {
         if (min > 0 && hora >= 18) {
-            return false    
+            return false
         }
 
-        return true
+        if (diaSemana >= 1 && diaSemana <= 5) {
+            return true
+        }
     }
 
     return false
 }
 
-const aberto = taAberto(new Date(2020, 3, 30, 7, 59))
+const aberto = taAberto(new Date(2021,3,25,18))
 
 console.log(aberto)
