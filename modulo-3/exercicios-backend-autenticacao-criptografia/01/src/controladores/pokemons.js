dc0b0d7537f4f4db7b8979827c985ab980be27c4
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-09-30 10:34:09 -0300

    Exercicios de criptografia e autenticação resolvido

--- modulo-3/exercicios-backend-autenticacao-criptografia/01/src/controladores/pokemons.js
+++ modulo-3/exercicios-backend-autenticacao-criptografia/01/src/controladores/pokemons.js
@@ -12,6 +12,11 @@ const cadastrarPokemon = async (req, res) => {
         return res.status(400).json({mensagem: 'As abilidades são obrigatórias'})
     }
 
+    let habilidadesFormatada = habilidades.split(',')
+    habilidadesFormatada = habilidadesFormatada.map((habilidade) => {
+        return habilidade.trim()
+    })
+
     try {
         const { id } = req.usuario
 
@@ -19,7 +24,7 @@ const cadastrarPokemon = async (req, res) => {
         (nome, usuario_id, apelido, habilidades, imagem)
         values
         ($1, $2, $3, $4, $5) returning *
-        `, [nome, id, apelido, habilidades, imagem])
+        `, [nome, id, apelido, habilidadesFormatada, imagem])
 
         return res.status(201).json(novoPokemon.rows[0])
     } catch (erro) {
@@ -27,6 +32,87 @@ const cadastrarPokemon = async (req, res) => {
     }
 }
 
+const atualizarApelido = async (req, res) => {
+    const { apelido } = req.body
+    const { id } = req.params
+
+    if (!apelido) {
+        return res.status(400).json({mensagem: 'Informar o apelido é obrigatório'})
+    }
+
+    if (isNaN(id)) {
+        return res.status(400).json({mensagem: 'O id não é um numero válido'})
+    }
+
+    try {
+        const novoApelido = await pool.query('update pokemons set apelido = $1 where id = $2 returning *', [apelido, id])
+
+        if (novoApelido.rowCount < 1) {
+            return res.status(404).json({mensagem: 'Pokemon não encontrado'})
+        }
+
+        return res.status(200).json(novoApelido.rows[0])
+    } catch (erro) {
+        return res.status(500).json({mensagem: erro.message})
+    }
+}
+
+const listarPokemons = async (req, res) => {
+    try {
+        const pokemons = await pool.query('select * from pokemons')
+
+        return res.json(pokemons.rows)
+    } catch (erro) {
+        return res.status(500).json({mensagem: erro.message})
+    }
+}
+
+const detalharPokemon = async (req, res) => {
+    const { id } = req.params
+
+    if (isNaN(id)) {
+        return res.status(400).json({mensagem: 'O id não é um numero válido'})
+    }
+
+    try {
+        const pokemon = await pool.query('select * from pokemons where id = $1', [id])
+
+        if (pokemon.rowCount < 1) {
+            return res.status(404).json({mensagem: 'Pokemon não encontrado'})
+        }
+
+        return res.json(pokemon.rows[0])
+    } catch (erro)  {
+        return res.status(500).json({mensagem: erro.message})
+    }
+}
+
+const deletarPokemon = async (req, res) => {
+    const { id } = req.params
+
+    if (isNaN(id)) {
+        return res.status(400).json({mensagem: 'O id não é um numero válido'})
+    }
+
+    try {
+        const { rowCount } = await pool.query('select * from pokemons where id = $1', [id])
+
+        if (rowCount < 1) {
+            return res.status(404).json({mensagem: 'Pokemon não encontrado'})
+        }
+
+        const pokemonDeletado = await pool.query('delete from pokemons where id = $1 returning *', [id])
+
+        return res.json(pokemonDeletado.rows[0])
+    } catch (erro) {
+        return res.status(500).json({mensagem: erro.message})
+    }
+}
+
 module.exports = {
-    cadastrarPokemon
+    cadastrarPokemon,
+    atualizarApelido,
+    listarPokemons,
+    detalharPokemon,
+    deletarPokemon
 }
--- modulo-3/exercicios-backend-autenticacao-criptografia/01/src/rotas.js
+++ modulo-3/exercicios-backend-autenticacao-criptografia/01/src/rotas.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const { cadastrarUsuario, login } = require('./controladores/usuarios')
-const { cadastrarPokemon } = require('./controladores/pokemons')
+const {
+        cadastrarPokemon,
+        atualizarApelido,
+        listarPokemons,
+        detalharPokemon,
+        deletarPokemon 
+    } = require('./controladores/pokemons')
 const verificarUsuario = require('./intermediarios/autenticacao')
 
 const rotas = express()
@@ -10,5 +16,9 @@ rotas.post('/login', login)
 
 rotas.use(verificarUsuario)
 rotas.post('/pokemon', cadastrarPokemon)
+rotas.patch('/pokemon/:id', atualizarApelido)
+rotas.get('/pokemon', listarPokemons)
+rotas.get('/pokemon/:id', detalharPokemon)
+rotas.delete('/pokemon/:id', deletarPokemon)
 
 module.exports = rotas
